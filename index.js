9095bcf4f0d1ca8a7adca0bfc4fd89aed758641f
rubyclaroreilly@gmail.com <rubyclaroreilly@gmail.com>
2018-07-20 00:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 function destructivelyAppendKitten(name){
   return kittens.push(name)
-  
 }
 
 function destructivelyPrependKitten(name){
   return kittens.unshift(name)
-  
 }
 
 function destructivelyRemoveLastKitten(){
   return kittens.pop(name)
-  
 }
 
 function destructivelyRemoveFirstKitten(){
   return kittens.shift(name)
-  
 }
 
 function appendKitten(name){
